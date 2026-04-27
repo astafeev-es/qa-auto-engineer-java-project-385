35bff15393b8aef97c228232c0c33234093945a6
Alexey Stafeev <a.stafeev@clickadu.com>
2026-04-27 16:46:53 +0200

    fixed gradle env

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-    environment(System.getenv())
+    // Указываем Gradle, что результат теста зависит от этой переменной
+    inputs.property("APP_BASE_URL", providers.environmentVariable("APP_BASE_URL").getOrElse("http://localhost:5173"))
+    
+    environment("APP_BASE_URL", providers.environmentVariable("APP_BASE_URL").getOrElse("http://localhost:5173"))
+    environment("USERNAME", providers.environmentVariable("USERNAME").getOrElse("aleksei98"))
+    environment("PASSWORD", providers.environmentVariable("PASSWORD").getOrElse("[trcktnghjtrn_98"))
 }
 
 tasks.withType<DependencyUpdatesTask> {
