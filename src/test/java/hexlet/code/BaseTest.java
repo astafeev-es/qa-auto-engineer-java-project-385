d235725354f54ba2529204e4d2aa4130a9941590
Alexey Stafeev <a.stafeev@clickadu.com>
2026-04-27 15:18:22 +0200

    fixed base url frrom env

--- src/test/java/hexlet/code/BaseTest.java
+++ src/test/java/hexlet/code/BaseTest.java
@@ -18,9 +18,20 @@ public abstract class BaseTest {
 
     @BeforeEach
     public void setUp() {
-        baseUrl = System.getProperty("APP_BASE_URL", "http://localhost:5173");
-        username = System.getProperty("USERNAME", "aleksei98");
-        password = System.getProperty("PASSWORD", "[trcktnghjtrn_98");
+        baseUrl = System.getProperty("APP_BASE_URL", System.getenv("APP_BASE_URL"));
+        if (baseUrl == null) {
+            baseUrl = "http://localhost:5173";
+        }
+
+        username = System.getProperty("USERNAME", System.getenv("USERNAME"));
+        if (username == null) {
+            username = "aleksei98";
+        }
+
+        password = System.getProperty("PASSWORD", System.getenv("PASSWORD"));
+        if (password == null) {
+            password = "[trcktnghjtrn_98";
+        }
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
